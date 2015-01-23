e04401b561e529c5641c0982a5a11c37b5eb68a0
ZiniaSult <sultana.hiramony@gmail.com>
2015-01-23 10:46:39 +0600

    new text is added

--- src/com/example/Beautiful_Bangladesh/manu.java
+++ src/com/example/Beautiful_Bangladesh/manu.java
@@ -4,11 +4,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by Sazzad on 1/22/2015.
  */
 public class manu extends Activity implements View.OnClickListener{
+    Button a1,a2,a3,a4,a5;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
